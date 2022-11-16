8b9125c0129c3150af12949dc5d595d100eb982e
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-11-16 19:14:45 +0200

    Add associations and validations

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,5 @@
 class Food < ApplicationRecord
+  belongs_to :user
+  has_many :recipe_foods, dependent: :destroy
+  validates :name, presence: true
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,5 @@
 class Recipe < ApplicationRecord
+  belongs_to :user, foreign_key: :user_id
+  has_many :recipe_foods, dependent: :destroy
+  validates :name, presence: true, length: { in: 3..200 }
 end
