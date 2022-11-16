d9221b48e88f50642f474963a9528a0408be4b29
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-11-17 00:07:30 +0200

    Add styles and modify shopping list

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   get 'recipes/public_recipes'
    devise_for :users
    devise_scope :user do  
-  #  get '/users/sign_out' => 'devise/sessions#destroy'     
+     get '/users/sign_out' => 'devise/sessions#destroy'     
    end
   resources :foods
   resources :recipefoods
   resources :recipes
-    
+  get 'shopping_list', to: 'shopping_lists#index' 
    # Defines the root path route ("/")
   root 'recipes#index'
 end
