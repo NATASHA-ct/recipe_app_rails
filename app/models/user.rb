625c0b9d34dcf26f754e82c9a0edb43a9b964671
Ntare Guy <gntare2@gmail.com>
2022-11-17 16:44:20 +0200

    refactor food page and delete button

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   has_many :foods, dependent: :destroy
   has_many :recipes, dependent: :destroy
 
-  validates :name, presence: true
+  validates :name, presence: false
 end
